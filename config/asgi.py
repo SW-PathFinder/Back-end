ee57eb46c3710e80f0340e7fba2b353dfaa83ae1
Dami <onemoredami@gmail.com>
2025-05-21 18:29:56 +0900

    modify settings path and set ASGI application asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -8,9 +8,29 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+# settings 경로 수정 (dev 환경 사용하는 경우)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
+# 아래는 기존 내용
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+
+django.setup()
 
 application = get_asgi_application()
+
+import apps.saboteur.routing
+
+
+# ASGI application 설정
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            apps.saboteur.routing.websocket_urlpatterns
+        )
+    ),
+})
