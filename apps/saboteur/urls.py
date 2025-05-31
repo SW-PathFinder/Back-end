34052e16091478445bdb9cf88ec0e6d41c1a564f
Nutria <81311826+Nutriatree@users.noreply.github.com>
2025-06-01 07:26:43 +0900

    Revert "Swpf 156 be 음성채팅 세션 생성 요청 api"

--- apps/saboteur/urls.py
+++ apps/saboteur/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.game_list, name='game_list'),
-    path("voice/", include("apps.saboteur.voiceChat.urls")),
 ]
